3f1099dafc5b900f1953474e00619c5f4b6324ff
Luis Carlos <92837088+LuisCarlosS@users.noreply.github.com>
2025-08-29 15:20:04 -0300

    Update newsletter.php

--- catalog/controller/extension/module/newsletter.php
+++ catalog/controller/extension/module/newsletter.php
@@ -2,64 +2,61 @@
 class ControllerExtensionModuleNewsletter extends Controller {
 	public function index($setting) {
 		$this->load->language('extension/module/newsletter');
-
 		$this->document->addStyle('catalog/view/theme/default/stylesheet/extension/module/newsletter.css');
 
-		$this->load->model('tool/image');
-
-		if (!$setting['name']) {
-			$data['name'] = "Newsletter";
-		}else{
-			$data['name'] = $setting['name'];
-		}
-
-		if (!$setting['title']) {
-			$data['title'] = "INSCREVA-SE E FIQUE POR DENTRO DE NOSSAS <em>NOVIDADES, OFERTAS ESPECIAIS</em> E MUITO MAIS!";
-		}else{
-			$data['title'] = $setting['title'];
-		}
-
-		if (!$setting['description']) {
-			$data['description'] = "";
-		}else{
-			$data['description'] = $setting['description'];
-		}
-
-		if (!$setting['placeholder']) {
-			$data['placeholder'] = "";
-		}else{
-			$data['placeholder'] = $setting['placeholder'];
-		}
-
-		if (!$setting['button']) {
-			$data['button'] = "";
-		}else{
-			$data['button'] = $setting['button'];
-		}
+		$data['name'] = !empty($setting['name']) ? $setting['name'] : 'Newsletter';
+		$data['title'] = !empty($setting['title']) ? $setting['title'] : 'INSCREVA-SE E FIQUE POR DENTRO DE NOSSAS <em>NOVIDADES, OFERTAS ESPECIAIS</em> E MUITO MAIS!';
+		$data['description'] = !empty($setting['description']) ? $setting['description'] : '';
+		$data['placeholder'] = !empty($setting['placeholder']) ? $setting['placeholder'] : '';
+		$data['button'] = !empty($setting['button']) ? $setting['button'] : '';
 
 		return $this->load->view('extension/module/newsletter', $data);
-		
 	}
 
-	public function cadastrar() {
-
-		$email = $this->request->post['email'];
-		$date = date('y/m/d');
-
-		$this->load->model('extension/module/newsletter');
-
-		$this->model_extension_module_newsletter->createTable();
-		$this->model_extension_module_newsletter->insere($email, $date);
-
+	public function register() {
+		$this->load->language('extension/module/newsletter');
 		$json = array();
 
-		$json[] = array(
-			'status' => "created",
-			'email'       => $email
-		);
+		if (isset($this->request->post['email']) && !empty(trim($this->request->post['email']))) {
+			$email = trim($this->request->post['email']);
+			
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$json[] = array(
+					'status' => 'error',
+					'message' => $this->language->get('error_email_invalid')
+				);
+			} else {
+				$this->load->model('extension/module/newsletter');
+				
+				try {
+					$result = $this->model_extension_module_newsletter->register($email, date('Y-m-d'));
+
+					if ($result) {
+						$json[] = array(
+							'status' => 'created',
+							'email' => $email
+						);
+					} else {
+						$json[] = array(
+							'status' => 'error',
+							'message' => $this->language->get('error_email_exists')
+						);
+					}
+				} catch (Exception $e) {
+					$json[] = array(
+						'status' => 'error',
+						'message' => $this->language->get('error_general')
+					);
+				}
+			}
+		} else {
+			$json[] = array(
+				'status' => 'error',
+				'message' => $this->language->get('error_email_required')
+			);
+		}
 
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
-
 	}
 }
