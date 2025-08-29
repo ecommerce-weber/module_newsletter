5befb8ae2ca7a9c697af90f83185126a3d1e54f4
Luis Carlos <92837088+LuisCarlosS@users.noreply.github.com>
2025-08-29 15:20:55 -0300

    Update newsletter.php

--- catalog/model/extension/module/newsletter.php
+++ catalog/model/extension/module/newsletter.php
@@ -1,18 +1,13 @@
 <?php
 class ModelExtensionModuleNewsletter extends Model {
-    public function createTable() {
-        $this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "newsletter` (
-            `newsletter_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-            `email` VARCHAR(255) NOT NULL,
-            `created` DATE NOT NULL,
-            PRIMARY KEY (`newsletter_id`)
-        ) ENGINE=MyISAM DEFAULT CHARSET=utf8");
-   
+    public function register($email, $date) {
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "newsletter WHERE email = '" . $this->db->escape($email) . "'");
+        
+        if ($query->num_rows == 0) {
+            $this->db->query("INSERT INTO " . DB_PREFIX . "newsletter SET email = '" . $this->db->escape($email) . "', created = '" . $this->db->escape($date) . "'");
+            return true;
+        }
+        
+        return false;
     }
-
-    public function insere($email, $date) {
-		
-		$this->db->query("INSERT INTO " . DB_PREFIX . "newsletter SET email = '" . $this->db->escape($email) . "', 	created = '" . $this->db->escape($date) . "'");
-	
-	}
 }
