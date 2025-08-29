c149331054530f65376e59b05f2d7c655712b552
Luis Carlos <92837088+LuisCarlosS@users.noreply.github.com>
2025-08-29 15:22:31 -0300

    Update newsletter.php

--- admin/model/extension/module/newsletter.php
+++ admin/model/extension/module/newsletter.php
@@ -1,8 +1,6 @@
 <?php
-class ModelExtensionModuleNewsletter extends Model {
-    
+class ModelExtensionModuleNewsletter extends Model {    
     public function getEmail() {
-
 		$sql = "SELECT email, created FROM " . DB_PREFIX . "newsletter ORDER BY created DESC";
 
 		$query = $this->db->query($sql);
@@ -10,8 +8,7 @@ class ModelExtensionModuleNewsletter extends Model {
 		return $query->rows;
 	}
 
-    public function verificaTabela() {
-
+    public function checkTable() {
         $sql = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME  = '" . DB_PREFIX . "newsletter'";
         
 		$query = $this->db->query($sql);
@@ -19,4 +16,12 @@ class ModelExtensionModuleNewsletter extends Model {
 		return $query->rows;
 	}
 
+    public function install() {
+        $this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "newsletter` (
+            `newsletter_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+            `email` VARCHAR(255) NOT NULL,
+            `created` DATE NOT NULL,
+            PRIMARY KEY (`newsletter_id`)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8");
+    }
 }
