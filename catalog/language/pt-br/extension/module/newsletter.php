3593d94c5b0d6999b4729ca01a0156eb866ca20e
Luis Carlos <92837088+LuisCarlosS@users.noreply.github.com>
2025-08-29 15:21:17 -0300

    Update newsletter.php

--- catalog/language/pt-br/extension/module/newsletter.php
+++ catalog/language/pt-br/extension/module/newsletter.php
@@ -1,3 +1,7 @@
 <?php
-// Heading
-$_['heading_title'] = 'Newsletter';
+$_['text_loading']            = 'Enviando...';
+$_['text_success']            = 'E-mail cadastrado com sucesso!';
+$_['error_email_required']    = 'Por favor, insira seu endereço de e-mail.';
+$_['error_email_invalid']     = 'Por favor, insira um endereço de e-mail válido.';
+$_['error_email_exists']      = 'E-mail já cadastrado em nosso sistema.';
+$_['error_general']           = 'Erro ao cadastrar e-mail. Tente novamente.';
